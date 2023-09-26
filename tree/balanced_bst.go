5a0292b1f07bc983a849a8ed2c4747a0cdb6011c
HDL Project <hdl.project.co@gmail.com>
2023-09-27 03:20:38 +0700

    feat: add rotate left and right implementation and test

--- tree/balanced_bst.go
+++ tree/balanced_bst.go
@@ -1,6 +1,8 @@
 package tree
 
-import "math"
+import (
+	"math"
+)
 
 type BalancedBST struct {
 	root *BST
@@ -18,12 +20,11 @@ func (b *BalancedBST) IsBalanced() bool {
 }
 
 func (b *BalancedBST) isBalanced(curr *BST, height *int) bool {
-	var leftHeight, rightHeight int
-
 	if curr == nil {
 		return true
 	}
 
+	var leftHeight, rightHeight int
 	isLeftBalanced := b.isBalanced(curr.left, &leftHeight)
 	isRightBalanced := b.isBalanced(curr.right, &rightHeight)
 
@@ -34,4 +35,75 @@ func (b *BalancedBST) isBalanced(curr *BST, height *int) bool {
 	}
 
 	return false
+}
+
+func (b *BalancedBST) GetImbalancedNode() *BST {
+	var height int
+	return b.getImbalancedNode(b.root, &height)
+}
+
+func (b *BalancedBST) getImbalancedNode(curr *BST, height *int) *BST {
+	if curr == nil {
+		return nil
+	}
+
+	var leftHeight, rightHeight int
+	imbalancedLeftNode := b.getImbalancedNode(curr.left, &leftHeight)
+	if imbalancedLeftNode != nil {
+		return curr.left
+	}
+	imbalancedRightNode := b.getImbalancedNode(curr.right, &rightHeight)
+	if imbalancedRightNode != nil {
+		return curr.right
+	}
+
+	*height = int(math.Max(float64(leftHeight), float64(rightHeight))) + 1
+
+	if math.Abs(float64(leftHeight-rightHeight)) <= 1 {
+		return nil
+	}
+
+	return curr
+}
+
+func (b *BalancedBST) RotateLeft(curr *BST) {
+	if curr.parent != nil {
+		if curr.parent.right == curr {
+			curr.parent.right = curr.right
+		} else if curr.parent.left == curr {
+			curr.parent.left = curr.right
+		}
+
+		curr.right.parent = curr.parent
+	}
+
+	curr.parent = curr.right
+
+	if curr.parent.left != nil {
+		curr.parent.left.parent = curr
+	}
+	curr.right = curr.parent.left
+
+	curr.parent.left = curr
+}
+
+func (b *BalancedBST) RotateRight(curr *BST) {
+	if curr.parent != nil {
+		if curr.parent.left == curr {
+			curr.parent.left = curr.left
+		} else if curr.parent.right == curr {
+			curr.parent.right = curr.left
+		}
+
+		curr.left.parent = curr.parent
+	}
+
+	curr.parent = curr.left
+
+	if curr.parent.right != nil {
+		curr.parent.right.parent = curr
+	}
+	curr.left = curr.parent.right
+
+	curr.parent.right = curr
 }
--- tree/balanced_bst_test.go
+++ tree/balanced_bst_test.go
@@ -1,6 +1,8 @@
 package tree
 
-import "testing"
+import (
+	"testing"
+)
 
 /* imbalanced tree
 			5
@@ -14,7 +16,7 @@ import "testing"
  	1	3		7	9
 		  4	  6
 */
-func TestBalancedBST(t *testing.T) {
+func TestBalancedBST_GetImbalancedNode(t *testing.T) {
 	parentNode := CreateBST(5)
 	parentNode.Insert(CreateBST(2))
 	parentNode.Insert(CreateBST(8))
@@ -28,6 +30,11 @@ func TestBalancedBST(t *testing.T) {
 		t.Fatalf("expect is_balanced %v but got %v", false, isBalanced)
 	}
 
+	imbalancedNode := balancedBST.GetImbalancedNode()
+	if imbalancedNode == nil {
+		t.Fatalf("expect not nil imbalanced_node")
+	}
+
 	parentNode.Insert(CreateBST(7))
 	parentNode.Insert(CreateBST(9))
 	parentNode.Insert(CreateBST(6))
@@ -35,5 +42,78 @@ func TestBalancedBST(t *testing.T) {
 	isBalanced = balancedBST.IsBalanced()
 	if !isBalanced {
 		t.Fatalf("expect is_balanced %v but got %v", true, isBalanced)
+	}
+
+	imbalancedNode = balancedBST.GetImbalancedNode()
+	if imbalancedNode != nil {
+		t.Fatalf("expect nil imbalanced_node")
+	}
+}
+
+/*
+	 imbalanced tree
+				5
+		  3			  8
+	 	2	4
+	  1
+*/
+/*
+	 after rotating
+				3
+		  2			  5
+	 	1			4	8
+*/
+func TestBalancedBST_RotateRight(t *testing.T) {
+	parentNode := CreateBST(5)
+	parentNodeAfterRotation := CreateBST(3)
+	parentNode.Insert(parentNodeAfterRotation)
+	parentNode.Insert(CreateBST(8))
+	parentNode.Insert(CreateBST(2))
+	parentNode.Insert(CreateBST(4))
+	parentNode.Insert(CreateBST(1))
+
+	balancedBST := CreateBalancedBST(parentNode)
+	balancedBST.RotateRight(parentNode)
+
+	bstDFS := CreateBSTDFS(parentNodeAfterRotation)
+	res := bstDFS.Traverse()
+
+	expectedRes := "3 2 1 5 4 8"
+	if res != expectedRes {
+		t.Fatalf("expect res %s but got %s", expectedRes, res)
+	}
+}
+
+/*
+	 imbalanced tree
+				2
+		  1			  4
+					3	5
+						  8
+*/
+/*
+	 after rotating
+				4
+		  2			  5
+	 	1	3			8
+*/
+func TestBalancedBST_RotateLeft(t *testing.T) {
+	parentNode := CreateBST(2)
+	parentNodeAfterRotation := CreateBST(4)
+	parentNode.Insert(parentNodeAfterRotation)
+	parentNode.Insert(CreateBST(1))
+	parentNode.Insert(CreateBST(5))
+	parentNode.Insert(CreateBST(3))
+	parentNode.Insert(CreateBST(8))
+
+	balancedBST := CreateBalancedBST(parentNode)
+	balancedBST.RotateLeft(parentNode)
+
+	bstDFS := CreateBSTDFS(parentNodeAfterRotation)
+	res := bstDFS.Traverse()
+
+	expectedRes := "4 2 1 3 5 8"
+	if res != expectedRes {
+		t.Fatalf("expect res %s but got %s", expectedRes, res)
 	}
 }
